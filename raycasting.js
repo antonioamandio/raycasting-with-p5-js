6580b8f7066b28d82eb6411864219977434150db
António Amândio <antonioamandio22@gmail.com>
2026-04-08 21:16:23 +0100

    feat: create a variable to camera plane

--- raycasting.js
+++ raycasting.js
@@ -16,6 +16,7 @@ const gameHeight = 200;
 
 let pos;
 let dir;
+let cameraPlane;
 
 function setup() {
   createCanvas(gameWidth, gameHeight);
@@ -23,10 +24,11 @@ function setup() {
 
   pos = createVector(5, 5);
   dir = createVector(0, -1);
+  cameraPlane = createVector(0.66, 0);
 }
 
 function draw() {
   background(190, 190, 255);
   fill(130);
-  rect(0, gameHeight / 2, gameWidth, gameHeight / 2);
+  rect(0, height / 2, width, height / 2);
 }
